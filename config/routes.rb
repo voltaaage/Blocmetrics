b13c2623a462f3f996d52310f9d13295868c17c1
Fong <fong.fan.90@gmail.com>
2015-08-30 18:56:46 -0500

    user sign in and out configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
-  get 'welcome/about'
-
   root to: 'welcome#index'
+
+  resources :users, only: [:update] 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create admin
+admin = User.new(
+  name: 'Administrator',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+admin.skip_confirmation!
+admin.save!
+
+# Create premium
+premium = User.new(
+  name: 'Premium Member',
+  email: 'premium@example.com',
+  password: 'helloworld',
+  role: 'premium'
+)
+premium.skip_confirmation!
+premium.save!
+
+# Create standard
+standard = User.new(
+  name: 'Standard User',
+  email: 'standard@example.com',
+  password: 'helloworld',
+  role: 'standard'
+)
+standard.skip_confirmation!
+standard.save!
+
+# Create users
+10.times do
+  user = User.new(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: 'helloworld'
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
+
+puts "#{User.count} users were created."
